c18c62c1dde034a1ad2204d63abc23d2f14843ba
tanishk15sharma <tanishk1582002@gmail.com>
2022-04-12 12:14:00 +0530

    link : sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,7 +1,44 @@
 import React from "react";
-
+import "./Sidebar.scss";
+import { Link } from "react-router-dom";
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <aside className="sidebar">
+      <ul className="sidebar-list">
+        <Link to="/">
+          <li>
+            <span className="material-icons icon"> home </span>
+            HOME
+          </li>
+        </Link>
+        <Link to="/label">
+          <li>
+            <span className="material-icons icon">label </span>
+            LABELS
+          </li>
+        </Link>
+        <Link to="/archive">
+          <li>
+            <span className="material-icons icon"> archive </span>
+            ARCHIVE
+          </li>
+        </Link>
+        <Link to="trash">
+          <li>
+            <span className="material-icons icon"> delete </span>
+            TRASH
+          </li>
+        </Link>
+
+        <Link to="/">
+          <li>
+            <span className="material-icons icon"> account_circle </span>
+            PROFILE
+          </li>
+        </Link>
+      </ul>
+    </aside>
+  );
 };
 
 export { Sidebar };

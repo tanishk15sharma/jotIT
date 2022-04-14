2dd23416d0a3713d96f9204d7b0643f314dec384
tanishk15sharma <tanishk1582002@gmail.com>
2022-04-14 09:20:31 +0530

    fix : style

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -6,7 +6,7 @@ const Login = ({ toggleAuth }) => {
     password: "",
   });
 
-  const [loginErrors, setLoginEErrors] = useState({
+  const [loginErrors, setLoginErrors] = useState({
     email: "",
     password: "",
     others: "",
@@ -14,41 +14,42 @@ const Login = ({ toggleAuth }) => {
 
   const loginInputHandler = (e) => {
     setLogin((data) => ({ ...data, [e.target.name]: e.target.value }));
+    setLoginErrors((err) => ({ ...err, [e.target.name]: "" }));
   };
   console.log(login);
   return (
-    <>
-      <div className="flex-cl h30">
-        <div className="border-bottom">
-          <button className=" w50 border-rg font-lg pd">LOG IN</button>
-          <button
-            className=" w50 font-lg border-reset pd"
-            onClick={() => toggleAuth(false)}
-          >
-            SIGNUP
-          </button>
-        </div>
-        <input
-          type="text"
-          placeholder="EMAIL"
-          name="email"
-          className="pd mg-rl-2 mg-top-1"
-          onChange={(e) => loginInputHandler(e)}
-          value={login.email}
-        />
-        <input
-          type="text"
-          name="password"
-          placeholder="PASSWORD"
-          className="pd mg-rl-2
-            mg-bottom-1
-          "
-          value={login.password}
-          onChange={(e) => loginInputHandler(e)}
-        />
-        <button className="pd border-none">LOG IN</button>
+    <form className="flex-cl h30">
+      <div className="border-bottom">
+        <button className=" w50 border-rg font-lg pd">LOG IN</button>
+        <button
+          className=" w50 font-lg border-reset pd"
+          onClick={() => toggleAuth(false)}
+        >
+          SIGNUP
+        </button>
       </div>
-    </>
+      <input
+        type="text"
+        placeholder="EMAIL"
+        name="email"
+        className="pd mg-rl-2 mg-top-3"
+        onChange={(e) => loginInputHandler(e)}
+        value={login.email}
+      />
+      <input
+        type="text"
+        name="password"
+        placeholder="PASSWORD"
+        className="pd mg-rl-2 "
+        value={login.password}
+        onChange={(e) => loginInputHandler(e)}
+      />
+      <button className=" border-reset  mg-bottom-3 txt-start mg-left-2 txt-underline">
+        Test Credientials
+      </button>
+
+      <button className="pd border-none">LOG IN</button>
+    </form>
   );
 };
 
--- src/components/auth/Signup.jsx
+++ src/components/auth/Signup.jsx
@@ -12,10 +12,20 @@ const Signup = ({ toggleAuth }) => {
         </button>
         <button className=" w50 font-lg border-rg pd">SIGNUP</button>
       </div>
-      <input type="text" placeholder="FIRST NAME" className="pd mg-rl-2" />
+      <input
+        type="text"
+        placeholder="FIRST NAME"
+        className="pd mg-rl-2 mg-top-1"
+      />
       <input type="text" placeholder="LAST NAME" className="pd mg-rl-2" />
       <input type="text" placeholder="EMAIL" className="pd mg-rl-2" />
-      <input type="text" placeholder="PASSWORD" className="pd mg-rl-2" />
+      <input
+        type="text"
+        placeholder="PASSWORD"
+        className="pd mg-rl-2 
+      mg-bottom-1
+      "
+      />
       <button className="pd border-none">SIGN IN</button>
     </div>
   );

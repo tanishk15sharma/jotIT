9f4cef590bda3443d7e3fbd8381e4813613e496f
tanishk15sharma <tanishk1582002@gmail.com>
2022-04-15 17:43:19 +0530

    style : modal

--- src/components/note-modal/NoteModal.jsx
+++ src/components/note-modal/NoteModal.jsx
@@ -4,68 +4,81 @@ import "react-quill/dist/quill.snow.css";
 
 import "./NoteModal.scss";
 const NoteModal = ({ toggleModal }) => {
-  const [noteTitle, setNoteTitle] = useState("");
-  const [noteBody, setNoteBody] = useState("");
-  const [value, setValue] = useState("");
-  console.log(value);
+  const [noteDetails, setNoteDetails] = useState({
+    title: "",
+    body: "",
+    color: "",
+    isPinned: false,
+    tags: [],
+  });
+  console.log(noteDetails);
   return (
-    <div className="fixed-container" onClick={(e) => toggleModal(false)}>
+    <main className="fixed-container" onClick={(e) => toggleModal(false)}>
       <div className="note-modal" onClick={(e) => e.stopPropagation()}>
         <div className="mg-1 flex-spBt-center">
           <input
-            placeholder="title"
+            placeholder="Title"
             className="reset-input"
-            value={noteTitle}
-            onChange={(e) => setNoteTitle(e.target.value)}
+            value={noteDetails.title}
+            onChange={(e) =>
+              setNoteDetails((details) => ({
+                ...details,
+                title: e.target.value,
+              }))
+            }
           />
-          <i class="fa-solid fa-thumbtack"></i>
+          <span className="material-icons  rotate-left"> push_pin </span>
         </div>
         <ReactQuill
           theme="snow"
-          value={value}
+          value={noteDetails.body}
           className="mg-1"
-          onChange={setValue}
-          modules={{
-            toolbar: [
-              [{ header: "1" }, { header: "2" }, { font: [] }],
-              // [{ size: [] }],
-              ["bold", "italic", "underline", "strike", "blockquote"],
-              [{ align: [] }],
-              [{ color: [] }],
-              [{ list: "ordered" }, { list: "bullet" }],
-              ["link", "image", "code-block"],
-              ["clean"],
-            ],
-          }}
-          formats={[
-            "header",
-            "font",
-            "size",
-            "bold",
-            "italic",
-            "underline",
-            "strike",
-            "blockquote",
-            "color",
-            "background",
-            "list",
-            "bullet",
-            "indent",
-            "link",
-            "video",
-            "image",
-            "code-block",
-            "align",
-          ]}
+          onChange={(value) =>
+            setNoteDetails((details) => ({ ...details, body: value }))
+          }
+          modules={NoteModal.modules}
+          formats={NoteModal.formats}
         />
-
-        <div className="note-body mg-1">
-          <div>color</div>
-          <div>Label</div>
-        </div>
+        <footer className="modal-footer mg-1">
+          <span className="material-icons pd-rl-1">palette</span>
+          <span className="material-icons">label</span>
+          <button className="border-reset mg-left-1 pointer">Save</button>
+        </footer>
       </div>
-    </div>
+    </main>
   );
 };
 
 export { NoteModal };
+NoteModal.modules = {
+  toolbar: [
+    [{ header: "1" }, { header: "2" }, { font: [] }],
+    // [{ size: [] }],
+    ["bold", "italic", "underline", "strike", "blockquote"],
+    [{ align: [] }],
+    [{ color: [] }],
+    [{ list: "ordered" }, { list: "bullet" }],
+    ["link", "image", "code-block"],
+    ["clean"],
+  ],
+};
+NoteModal.formats = [
+  "header",
+  "font",
+  "size",
+  "bold",
+  "italic",
+  "underline",
+  "strike",
+  "blockquote",
+  "color",
+  "background",
+  "list",
+  "bullet",
+  "indent",
+  "link",
+  "video",
+  "image",
+  "code-block",
+  "align",
+];
